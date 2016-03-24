f59696a7dfbf03f733c13e780faa17e0b0920902
Wassim Dhif <wassimdhif@gmail.com>
2016-03-24 13:48:33 +0100

    jZoom wrapper fix

--- package.js
+++ package.js
@@ -1,13 +1,22 @@
 Package.describe({
-  name: 'wdhif:jzoom',
-  version: '1.0.0',
-  summary: 'jZoom Performant Image Magnifying Glass Effect wrapper - original jquerry plugin by https://github.com/pciapcib',
-  git: 'https://github.com/wdhif/meteor-jzoom',
-  documentation: 'README.md'
+    name: 'wdhif:jzoom',
+    version: '1.0.0',
+    summary: 'jZoom Performant Image Magnifying Glass Effect wrapper - original jquery plugin by https://github.com/pciapcib',
+    git: 'https://github.com/wdhif/meteor-jzoom',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('jzoom.min.js');
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
+
+    // Dependencies
+    Npm.depends({
+        'jquery': '2.2.2'
+    });
+
+    // Client files
+    api.addFiles([
+        'client/jzoom.min.js'
+    ], 'client');
 });
